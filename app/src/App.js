6d28b5b01ec5a7a60e0b9076b9b28a8b6e6f7422
Laurin Lukas Stahl <laurinstahl@googlemail.com>
2023-03-04 18:26:01 +0100

    setting up FE

---
+++ app/src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import api from './util/axios';
+import { useEffect, useState } from 'react';
+import { Alert, ChakraProvider, Box } from '@chakra-ui/react';
+import { theme, Button, IconButton }   from './theme/index';
+
+
+function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    api.get('/api/items')
+    .then(response => {
+      setItems(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Box p="16px" display="flex" flexDirection="row" gap="8px">
+            {items.map(item => {
+              return (
+                
+                  <Button colorScheme="blue" color="brand.900" key={item.id}>{item.name}</Button>
+                
+                )
+            })}
+          </Box>
+          <Box p="16px" display="flex" flexDirection="row" gap="8px">
+            {items.map(item => {
+              return (
+                
+                  <IconButton variant="primary" key={item.id}>{item.link}</IconButton>
+                
+                )
+            })}
+          </Box>
+          <Alert>Hello</Alert>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ChakraProvider>
+  );
+}
+
+export default App;
