dd3881e002176cdfcc6c81663b643ad0eb63c7cf
Laurin Lukas Stahl <laurinstahl@googlemail.com>
2023-03-05 01:02:01 +0100

    More changes

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,8 @@ import api from './util/axios';
 import { useEffect, useState } from 'react';
 import { ChakraProvider, Box } from '@chakra-ui/react';
 import { theme, Button, IconButton, Alert }   from './theme/index';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ItemList, ItemDetails } from './pages/index';
 
 function App() {
 
@@ -22,38 +23,16 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <div className="App">
-        <header className="App-header">
-          <Box p="16px" display="flex" flexDirection="row" gap="8px">
-            {items.map(item => {
-              return (
-                
-                  <Button colorScheme="blue" color="brand.900" key={item.id}>{item.name}</Button>
-                
-                )
-            })}
-          </Box>
-          <Box p="16px" display="flex" flexDirection="row" gap="8px">
-            {items.map(item => {
-              return (
-                
-                  <IconButton variant="primary" key={item.id}>{item.link}</IconButton>
-                
-                )
-            })}
-          </Box>
-          <Alert>Hello</Alert>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ItemList} />
+          <Route exact path="/item/:id" component={ItemDetails} />
+        </Switch>
+
+      </Router>
       </div>
-    </ChakraProvider>
-  );
+      </ChakraProvider>
+    );
 }
 
 export default App;
---
+++ app/src/pages/itemDetails.js
@@ -0,0 +1,52 @@
+import './App.css';
+import api from './util/axios';
+import { useEffect, useState } from 'react';
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import { theme, Button, IconButton, Alert }   from './theme/index';
+
+export const itemDetails = () => {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    api.get('/api/items')
+    .then(response => {
+      setItems(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
+
+  return (
+    <header className="App-header">
+      <Box p="16px" display="flex" flexDirection="row" gap="8px">
+        {items.map(item => {
+          return (
+            
+              <Button colorScheme="blue" color="brand.900" key={item.id}>{item.name}</Button>
+            
+            )
+        })}
+      </Box>
+      <Box p="16px" display="flex" flexDirection="row" gap="8px">
+        {items.map(item => {
+          return (
+            
+              <IconButton variant="primary" key={item.id}>{item.link}</IconButton>
+            
+            )
+        })}
+      </Box>
+      <Alert>Hello</Alert>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
